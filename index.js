61078e0d03e455aaa413bec3939e9e7ce66e1af7
Heta Joutsiniemi <pnkk@users.noreply.github.com>
2018-02-16 10:56:08 +0200

    Add button to be served on GET and improve logging

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = 9876;
 
@@ -10,25 +11,27 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 
 app
   .get('/', (req, res) => {
-    res.send("HELLO WORLD get received \n");
+    res.sendFile(path.join(__dirname + '/button.html'));
   })
   .post('/', function (req, res) {
-    console.log('!!! request body');
-    console.log(JSON.stringify(req.body, null, 2));
-
-    console.log('HEADERS:');
+    console.log('METHOD:', console.log(req.method));
+    console.log("\nHEADERS:");
     console.log('content-type', req.header('content-type'));
-
     console.log('PAYPAL-TRANSMISSION-SIG', req.header('PAYPAL-TRANSMISSION-SIG'));
     console.log('PAYPAL-AUTH-ALGO', req.header('PAYPAL-AUTH-ALGO'));
     console.log('PAYPAL-CERT-URL', req.header('PAYPAL-CERT-URL'));
+    console.log('PAYPAL-TRANSMISSION-ID', req.header('PAYPAL-TRANSMISSION-ID'));
+    console.log('PAYPAL-TRANSMISSION-TIME', req.header('PAYPAL-TRANSMISSION-TIME'));
+
+    console.log("\nBODY:");
+    console.log(JSON.stringify(req.body, null, 2));
 
     res.send("POST request to the homepage \n");
   })
   .listen(process.env.PORT || PORT, (error) => {
     if (error) {
       console.log('!!! ERROR: ', error);
-      return error;
+      throw error;
     };
     console.log(`Express server listening on port ${process.env.PORT || PORT}`);
   });
